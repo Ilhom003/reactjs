0a32a55f4b6587a884cdcf6d414c5379ddd00348
Ilhom Abdulahatov <abdulahatov03gmail.com>
2022-12-29 19:24:38 +0300

    some changes

---
+++ src/components/Ilhom/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const Ilhom = () => {
+    return (
+        <div>
+            <h1>Hi from Ilhom's Component</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, reprehenderit!</p>
+        </div>
+    )
+}
+
+export default Ilhom
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Ilhom from './components/Ilhom'
+import Islom from './components/Islom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const str = false
 root.render(
   <React.StrictMode>
-    <App />
+    <Ilhom />
+    <Islom />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+// const title='hi react'
+// ReactDOM.render(<h1>{title}</h1>, document.getElementById("root"))
+// const title=<h1>hi react</h1>;
+// ReactDOM.render(title, document.getElementById("root"))
